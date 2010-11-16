671d79de85e061fd343cfdc00f1b0f12a047b1d1
Mark Bell <me@markashleybell.com>
2010-11-16 15:50:56 +0000

    Commenting

--- wordstats.py
+++ wordstats.py
@@ -1,25 +1,36 @@
-import urllib, re
+import urllib, re, sys
 from operator import itemgetter
 from BeautifulSoup import BeautifulSoup, NavigableString
 
-file = urllib.urlopen('http://www.dailymail.co.uk/home/index.html')
-html = file.read()
-file.close()
+# Get the list of stop words from our text file
+file = open('stopwords.txt', 'r')
+# Remember to strip the newline characters so they match our words
+stopwords = [str(line).rstrip() for line in file]
+
+# Get the raw HTML
+page = urllib.urlopen('http://www.dailymail.co.uk/home/index.html')
+html = page.read()
+page.close()
 
 soup = BeautifulSoup(html)
+# Get the text from specific tags
 text = [t.text for t in soup.findAll(['p', 'h1', 'h2', 'span', 'li'])]
-words = re.sub('[^a-zA-Z0-9\-\s]', '', ' '.join(text)).split(' ')
+# Remove all punctuation except hyphens and apostrophes
+words = re.sub('[^a-zA-Z0-9\-\s\']', '', ' '.join(text)).split(' ')
 
 wordcount = {}
 
 for word in words:
     w = word.lower()
-    if(w not in wordcount):
-        wordcount[w] = 0
-    wordcount[w] += 1
+    if w not in stopwords: # If it's a stop word, ignore it
+        if w not in wordcount: # If we haven't already seen the word
+            wordcount[w] = 0 # Add it to the dictionary
+        wordcount[w] += 1 # Increment the total occurrences
     
+# Get a list of the items sorted by descending value (occurrences)
 sorted = sorted(wordcount.items(), key=itemgetter(1), reverse=True)
 
+# Show everything with more than 5 occurrences
 for s in sorted:
-    if s[1] > 10:
+    if s[1] > 5:
         print s[0] + ': ' + str(s[1])
